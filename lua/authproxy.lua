d9a88ab4e76c4118a36b696166c2195a93d7491e
zonesan <san@zoobook.org>
2016-11-21 18:01:02 +0800

    lua srcipt.

--- lua/authproxy.lua
+++ lua/authproxy.lua
@@ -5,13 +5,13 @@ local redisClient = require "resty.redis"
 
 
 local function auth_by_basic(user_info)
-	ngx.log(ngx.NOTICE,"Authorization basic")
+    ngx.log(ngx.NOTICE,"Authorization basic")
     ngx.log(ngx.NOTICE, user_info)
     ngx.header["basic"]=user_info
 end
 
 local function auth_by_token(user_info)
-	ngx.log(ngx.NOTICE,"Authorization token")
+    ngx.log(ngx.NOTICE,"Authorization token")
     ngx.log(ngx.NOTICE, user_info)
     ngx.header["token"]=user_info
 end
@@ -27,25 +27,8 @@ if not auth_req then
     ngx.exit(401)
 end
 
-local auth = {}
+--  curl -i -H 'http-x-proxy-cas-loginname: hello' /
 
-for type, info in string.gmatch(auth_req, "(%w+)%s(%w+)") do
-    auth["type"] = type
-    auth["info"] = info
-end
-
-if auth['type'] == "Basic" then
-    -- ngx.say('Basic function')
-    auth_by_basic(auth['info'])
-
-    elseif auth['type'] == "Token" then
-        -- ngx.say('Token function')
-        auth_by_token(auth['info'])
-    else
-        --ngx.say(auth['type'])
-        ngx.status = 401
-        ngx.exit(401)
-    end
-
-    ngx.header["hello"]='waorld';
-    ngx.say(ngx.header);
+ngx.header["hello"]='waorld';
+ngx.header["Authorization"]="Bearer " .. auth_req
+ngx.say(ngx.header);
